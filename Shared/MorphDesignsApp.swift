ab6e17e6d5a5d7690389ecf226b6e57f9c6f2196
frimpongopoku <pongofrimi@gmail.com>
2020-12-27 14:53:23 +0000

    Initial Commit

---
+++ Shared/MorphDesignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphDesignsApp.swift
+//  Shared
+//
+//  Created by frimpongOpokuAgyemang on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct MorphDesignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
